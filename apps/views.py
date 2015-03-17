6755b89c1a732d61a16fcabaa6c85fd7ec1d6492
Alex Crawley <alexcrawley@outlook.com>
2015-03-17 00:30:37 +0000

    Add mailchimp form

--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,16 @@
 from django.views import generic
+from django.core.urlresolvers import reverse_lazy
+
+from apps.notifications.forms import SubscribeForm
+from apps.notifications.mailchimp_services import subscribe_email
 
 
-class EmptyView(generic.TemplateView):
+class LandingPageView(generic.FormView):
     template_name = 'landing_page.html'
+    form_class = SubscribeForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        subscribe_email(form.cleaned_data['email'])
+
+        return super(LandingPageView, self).form_valid(form)
--- urls.py
+++ urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
-from apps.views import EmptyView
+from apps.views import LandingPageView
 from apps import api_urls
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', EmptyView.as_view(), name='home'),
+    url(r'^$', LandingPageView.as_view(), name='home'),
     url(r'^api/', include(api_urls)),
 
     url(r'^admin/', include(admin.site.urls)),
