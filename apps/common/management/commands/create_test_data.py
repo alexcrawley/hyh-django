4beea14d749c4a230c49bc575f06b3b41869122d
Alex Crawley <alexcrawley@outlook.com>
2015-03-26 22:48:31 +0000

    Add organiser to events test data

--- apps/common/management/commands/create_test_data.py
+++ apps/common/management/commands/create_test_data.py
@@ -2,7 +2,7 @@
 from django.core.management.base import BaseCommand
 
 from apps.events.models import Event
-from apps.users.models import User
+from apps.users.models import User, Organiser
 
 
 class Command(BaseCommand):
@@ -31,15 +31,20 @@ class Command(BaseCommand):
                 )
 
     def create_events(self):
+        organiser = Organiser.objects.create(company_name='Test Organiser')
+
         Event.objects.create(
+            organiser=organiser,
             title='Firework tonight!',
             img='http://www.mathforgrownups.com/wp-content/uploads/2012/06/firework.jpg.pagespeed.ce.Fh58gK_YoW.jpg'
             )
         Event.objects.create(
+            organiser,
             title='Fine dining..',
             img='http://www.tablespoon.com/~/media/Images/Articles/qd/2011/07/2011-07-25-monster-veggie-burger-500.jpg'
             )
         Event.objects.create(
+            organiser,
             title='Rooftop cinema?',
             img='http://1.bp.blogspot.com/-2SzFzltjliM/Ua8oFQuBviI/AAAAAAAAADM/w7mOBWrbT-8/s1600/RooftopFilmClub.jpg'
             )
